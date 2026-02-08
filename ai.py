dbf2b6fe4fa8c79d1be519cfe0780c5934501aee
bygeldi <bygeldi3421@gmail.com>
2026-02-08 21:57:13 +0300

    Ilk yapay zeka kodum

---
+++ ai.py
@@ -0,0 +1,7 @@
+# Bu bir yapay zeka 'merhaba dünya' kodudur
+mesaj = "Seni seviyorum"
+
+if "seviyorum" in mesaj:
+    print("Analiz sonucu: POZİTİF (Mutlu) 😊")
+else:
+    print("Analiz sonucu: NEGATİF (Üzgün) 😔")
